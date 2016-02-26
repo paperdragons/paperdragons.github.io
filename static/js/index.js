8a41dc498961bba1fe052636a4a642376dc08d5c
Junchao Fu <fjc@ubuntu.lan>
2016-02-26 15:59:28 +0800

    Format html,css,js code.Implement category click.

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,37 @@
 /**
  * 页面ready方法
  */
-$(function(){
-    generateContent();
+$(function () {
+  categoryDisplay();
+  generateContent();
 });
+
+/**
+ * 分类展示
+ * 点击右侧的分类展示时
+ * 左侧的相关裂变展开或者收起
+ * @return {[type]} [description]
+ */
+function categoryDisplay() {
+  /*only show All*/
+  $(".content>div[cate!=All]").hide();
+
+  /*show category when click categories list*/
+  $(".cate-choose").click(function () {
+    var cate = $(this).attr("cate");
+    $(".content>div[cate!=" + cate + "]").hide(250);
+    $(".content>div[cate=" + cate + "]").show(400);
+  });
+}
 
 /**
  * 侧边目录
  */
 function generateContent() {
-    if (typeof $("#markdown-toc").html() === "undefined") {
-        $(".post-summary").hide();
-        $(".post").removeClass("col-sm-9").addClass("col-sm-12");
-    } else {
-        $(".post-summary .summary").html("<ul>" + $("#markdown-toc").html() + "</ul>");
-    }
+  if (typeof $("#markdown-toc").html() === "undefined") {
+    $(".post-summary").hide();
+    $(".post").removeClass("col-sm-9").addClass("col-sm-12");
+  } else {
+    $(".post-summary .summary").html("<ul>" + $("#markdown-toc").html() + "</ul>");
+  }
 }
