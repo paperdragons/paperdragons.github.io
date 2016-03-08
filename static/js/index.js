0a1348564acfb5ecf76110f49d3cbb3ebddb0dab
FJC <frank_fjc@qq.com>
2016-03-09 00:13:00 +0800

    Set the summary of post.

--- static/js/index.js
+++ static/js/index.js
@@ -2,33 +2,14 @@
  * 页面ready方法
  */
 $(function () {
-  categoryDisplay();
   generateContent();
 });
-
-/**
- * 分类展示
- * 点击右侧的分类展示时
- * 左侧的相关裂变展开或者收起
- * @return {[type]} [description]
- */
-function categoryDisplay() {
-  /*only show All*/
-  $(".content>div[class=list-group][cate!=All]").hide();
-
-  /*show category when click categories list*/
-  $(".cate-choose").click(function () {
-    var cate = $(this).attr("cate");
-    $(".content>div[cate!=" + cate + "]").hide(250);
-    $(".content>div[cate=" + cate + "]").show(400);
-  });
-}
 
 /**
  * 侧边目录
  */
 function generateContent() {
-  if (typeof $("#markdown-toc").html() === "undefined") {
+  if (typeof $("#markdown-toc").html() == "undefined") {
     $(".post-summary").hide();
     $(".post").removeClass("col-sm-9").addClass("col-sm-12");
   } else {
